2afb71ca050162b56981bd2844cd12cf7aa33497
Yoon-Jiyeon <she79560@naver.com>
2023-01-18 13:03:09 +0900

    [바킹독-스택의활용] BOJ_10799_쇠막대기(-지연)

---
+++ Baekjoon/Season_3/jiyeon/6_week/BOJ_10799_쇠막대기.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string bracketInfo;
+    getline(cin, bracketInfo);
+    stack<int> idxStack;
+    vector<int> lasers;
+    vector<pair<int, int>> sticks;
+    int idx = -1;
+    bool isLaser = false;
+
+    for(auto c: bracketInfo) {
+        idx++;
+        if(c == '(') {
+            isLaser = true;
+            idxStack.push(idx);
+        }
+        else {
+            if(!isLaser) {
+                sticks.push_back(make_pair(idxStack.top(), idx));
+                idxStack.pop();
+            } 
+            else {
+                isLaser = false;
+                lasers.push_back(idx-1);
+                idxStack.pop();
+            }
+        }
+    }
+    int result = 0;
+    for(int i = 0; i < lasers.size(); i++)
+        for(int j = 0; j < sticks.size(); j++) 
+            if(sticks[j].first <= lasers[i] && lasers[i] <= sticks[j].second) result++;
+    cout << result + sticks.size();
+}
