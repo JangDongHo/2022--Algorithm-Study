9eb776a5bcf7afd68bea5050521e998dc04d1fff
Jang-DongHo <donghoo18@naver.com>
2023-01-25 10:56:24 +0900

    백준 3986 좋은 단어

---
+++ Baekjoon/Season_3/dongho/week6/baekjoon_3986.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class baekjoon_3986 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    Stack<Character> stack = new Stack<Character>();
+    int count = 0;
+
+    int N = Integer.parseInt(br.readLine()); // 단어의 수
+
+    while (N-- > 0) {
+      boolean isGood = true;
+      String word = br.readLine();
+      for (int i = 0; i < word.length(); i++) {
+        Character alphabet = word.charAt(i);
+        // 스택의 최상단 값과 검사하려는 값이 같을 경우 pop
+        if (!stack.isEmpty() && stack.peek() == alphabet) {
+          stack.pop();
+        } else {
+          stack.push(alphabet);
+        }
+      }
+      if (stack.isEmpty()) {
+        count++;
+      }
+      stack.clear();
+    }
+    System.out.println(count);
+  }
+}
